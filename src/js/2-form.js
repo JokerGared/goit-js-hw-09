600611cde5fa56809e30516c23fb3d3365b5be22
Oleh <therkasoa@gmail.com>
2025-02-07 00:50:45 +0200

    make 9 hometask

---
+++ src/js/2-form.js
@@ -0,0 +1,55 @@
+'use strict';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  localKey: 'feedback-form-state',
+};
+const formData = { email: '', message: '' };
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function getFromLS(key) {
+  const lSData = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(lSData);
+    return data;
+  } catch (error) {
+    return lSData;
+  }
+}
+
+function fillInitForm() {
+  const { email = '', message = '' } = getFromLS(refs.localKey) || {};
+  refs.form.elements.email.value = email;
+  refs.form.elements.message.value = message;
+  formData.email = email;
+  formData.message = message;
+}
+
+function onSubmitPress(evt) {
+  evt.preventDefault();
+  const email = evt.currentTarget.elements.email.value.trim();
+  const message = evt.currentTarget.elements.message.value.trim();
+  if (email === '' || message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  evt.target.reset();
+  localStorage.removeItem(refs.localKey);
+  formData.email = '';
+  formData.message = '';
+}
+
+fillInitForm();
+
+refs.form.addEventListener('input', evt => {
+  const email = evt.currentTarget.elements.email.value.trim();
+  const message = evt.currentTarget.elements.message.value.trim();
+  formData.email = email;
+  formData.message = message;
+  saveToLS(refs.localKey, formData);
+});
+refs.form.addEventListener('submit', onSubmitPress);
